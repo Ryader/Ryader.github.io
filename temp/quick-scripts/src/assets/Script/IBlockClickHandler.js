cae0166d666bab758c51dc585f78ab43e9f5f4bd
Rayder <55925741+Ryader@users.noreply.github.com>
2026-05-24 13:34:34 +0300

    Delete temp directory

--- temp/quick-scripts/src/assets/Script/IBlockClickHandler.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-cc._RF.push(module, '53ce5tmXtdFp5l1+Xhw5Iwr', 'IBlockClickHandler');
-// Script/IBlockClickHandler.ts
-
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-
-cc._RF.pop();
