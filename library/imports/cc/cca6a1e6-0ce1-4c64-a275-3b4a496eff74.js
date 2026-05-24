9cd3ab590b63d6c75aa62bf40682c97139299ac8
Rayder <55925741+Ryader@users.noreply.github.com>
2026-05-24 13:34:46 +0300

    Delete library directory

--- library/imports/cc/cca6a1e6-0ce1-4c64-a275-3b4a496eff74.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-cc._RF.push(module, 'cca6aHmDOFMZKJ1O0pJbv90', 'IGridAccess');
-// Script/IGridAccess.ts
-
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-
-cc._RF.pop();
